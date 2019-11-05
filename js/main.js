24b9108fa8b7d9b1a1d1b192b17065fe8de002cb
IsakEriksson <isak1993@gmail.com>
2019-11-05 19:25:07 +0100

    blabla

--- js/main.js
+++ js/main.js
@@ -13,12 +13,17 @@
         }
     };
 
-    /*document.addEventListener("DOMContentLoaded", function(event){
-        var wrapper = document.getElementById("wrapper");
-        var wrapperOffset = wrapper.offsetHeight;
-        if(wrapperOffset < screen.height) {
-            document.getElementById("footer").classList.add("fixed-footer");
-        }
-    }, false);*/
-
 }())
+
+/*
+    Anser du din lösning vara produktionsredo, om inte, vad skulle behöva förbättras (tekniskt eller användarperspektiv)?
+
+    Svar:
+    Jag anser att min lösning är produktionsredo met två undantag:
+    - Kontaktformuläret skickar inget mail. Innan sidan sattes i produktion skulle man ange en mailadress för att ta
+      emot mailen och skapa stöd för ett mail att skickas då användaren klickar "Skicka".
+    - Tabellen över medlemmar uppdateras inte dynamiskt. Innan sidan sattes i produktion skulle man ha en underliggande
+      datastruktur som hämtar och uppdaterar tabellen med medlemmar automatiskt så att ingen behöver göra det manuellt.
+
+    I övrigt är sidan så simpel och de krav som ställs på den så små att jag anser att den är klar att sättas i produktion.
+*/
